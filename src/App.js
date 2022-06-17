6fe70c6ea408de93be2d511b1e62535ee3a08b79
Oksana Trygub <ok.trygub@gmail.com>
2022-06-17 17:34:35 +0300

    fix carousel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import Carousel from "./components/Carousel";
+// import firstImg from './images/one.jpg';
+// import secondImg from './images/two.jpg';
+// import thirdImg from './images/three.jpg';
 
 function App() {
     const paths = ['./images/one.jpg', './images/two.webp', './images/three.jpg'];
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,81 +1,50 @@
 import React from 'react';
-import cn from 'classnames';
 
 class Carousel extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isActive: 'active',
             pathIndex: 0,
         }
-
-        console.log(this.state.pathIndex);
     }
 
     changeImage = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
 
+        const images= this.props.images;
 
-        console.log(this.state.pathIndex);
+        if (event.target.className === 'carousel-control-next') {
+            this.setState((state) => ({pathIndex: state.pathIndex === images.length - 1 ? 0 : ++state.pathIndex}));
+        }
 
-
-        if (event.target.className === 'carousel-control-next') this.setState(state => ({pathIndex: ++state.pathIndex}));
-        if (event.target.className === 'carousel-control-prev') this.setState(state => ({pathIndex: --state.pathIndex}));
+        if (event.target.className === 'carousel-control-prev') {
+            this.setState((state) => ({pathIndex: state.pathIndex === 0 ? images.length - 1 : --state.pathIndex}));
+        }
         console.log(this.state.pathIndex)
-
-
-        // this.setState(state => ({isActive: 'active'}))
-        // setTimeout(() => console.log(this.state), 2000)
-
-        // this.setState(state => ({pathIndex: ++state.pathIndex}));
-        //
-        // console.log(this.state.pathIndex);
-
-
-
-        // const newIndex = event.target.className === 'carousel-control-next' ? this.state.pathIndex +1 :this.state.pathIndex -1
-        // this.setState({
-        //     pathIndex: newIndex
-        // })
-        // console.log(this.state.pathIndex);
-
     }
 
     render() {
         const {images} = this.props
-        // console.log(images)
-
-        const isActive = cn('carousel-item', {
-            'active': this.state.isActive === 'active',
-        });
-
 
         return (
             <div id="carousel" className='carousel slide' data-bs-ride="carousel">
                 <div className="carousel-inner">
-            {/*//         <div className={isActive} key={images.index}>*/}
-            {/*//             <img alt="" className="d-block w-100" src={images[this.state.pathIndex]}/>*/}
-            {/*//         </div>*/}
-            {/*//*/}
-
-                    <div className="carousel-item active">
-                        <img alt="" className="d-block w-100" src="./images/one.jpg"/>
+                    <div className='carousel-item active'>
+                        <img alt="" className="d-block w-100" src={images[this.state.pathIndex]}/>
                     </div>
 
                     <div className="carousel-item">
-                        <img alt="" className="d-block w-100" src="./images/two.webp"/>
+                        <img alt="" className="d-block w-100" src={images[this.state.pathIndex]}/>
                     </div>
 
                     <div className="carousel-item">
-                        <img alt="" className="d-block w-100" src="./images/three.jpg"/>
+                        <img alt="" className="d-block w-100" src={images[this.state.pathIndex]}/>
                     </div>
-
-              </div>
-
-
-
+                </div>
 
                 <button className="carousel-control-prev" data-bs-target="#carousel" type="button" data-bs-slide="prev"
-                        >
+                        onClick={this.changeImage}>
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Previous</span>
                 </button>
@@ -86,12 +55,10 @@ class Carousel extends React.Component {
                     <span className="visually-hidden">Next</span>
                 </button>
 
-
             </div>
-
-
         )
     }
+
 }
 
 export default Carousel;
