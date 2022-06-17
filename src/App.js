30404b6d9f8d00812c44418e5987dd44d10e0083
Oksana Trygub <ok.trygub@gmail.com>
2022-06-18 00:12:00 +0300

    edit MyForm

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import MyForm from "./components/MyForm";
 function App() {
     return (
         <div className="App">
-            {/*<MyForm text={'hi'} />*/}
             <MyForm />
         </div>
     );
