6a55ffa02168ca582f92bff12000695fe0102954
Oksana Trygub <ok.trygub@gmail.com>
2022-06-27 19:28:59 +0300

    add Modal component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const Body = (props) => <div className="modal-body">{props.children}</div>;
 
 export default Body;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const Footer = (props) => <div className="modal-footer">{props.children}</div>;
 
 export default Footer;
