9710646af044ff698ec74dd50c5a5787312616bc
Oksana Trygub <ok.trygub@gmail.com>
2022-06-27 16:23:17 +0300

    add

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import Header from "./Header";
+import Body from "./Body";
+import Footer from "./Footer";
 
+class Modal extends React.Component {
 
-
-export default class Modal extends React.Component {
-
+    static Header = Header;
+    static Body = Body;
+    static Footer = Footer;
 
     render() {
 
+
         return (
-            <div>
+            <div className="modal" role="dialog">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        {this.props.children}
+                    </div>
+                </div>
 
             </div>
         );
     }
 }
+
+export default Modal;
