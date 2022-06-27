97c21295a97fccccc61135bb8e3a8c2bccd80961
Oksana Trygub <ok.trygub@gmail.com>
2022-06-27 17:01:41 +0300

    add Modal component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ const Header = (props) => {
     return (
         <div className="modal-header">
             <div className="modal-title">{props.children}</div>
-            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close">
+            <button type="button" onClick={props.toggle} className="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close">
             </button>
         </div>
     )
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer";
+import cn from 'classnames';
 
 class Modal extends React.Component {
 
@@ -10,10 +11,23 @@ class Modal extends React.Component {
     static Footer = Footer;
 
     render() {
+        const {isOpen} = this.props;
+        console.log(isOpen)
 
+        const modalClass = cn('modal', {
+            'show': isOpen,
+            'fade': isOpen
+        })
+
+        const modalStyle = {
+            display: cn({
+                'block': isOpen,
+                'none': !isOpen,
+            })
+        }
 
         return (
-            <div className="modal" role="dialog">
+            <div className={modalClass} style={modalStyle} role="dialog">
                 <div className="modal-dialog">
                     <div className="modal-content">
                         {this.props.children}
