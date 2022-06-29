32d39ff06d8bc9961ccceba8a1120dd6de1b2bea
Oksana Trygub <ok.trygub@gmail.com>
2022-06-29 10:41:58 +0300

    add Todo component

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### An implementation of the `TodoBox component` with the ability to add and remove notes.
+#### An implementation of the `TodoBox` component with the ability to add and remove notes.
 
 The elements are added in the reverse order. The new ones are always on top.
 
--- src/components/TodoBox.jsx
+++ src/components/TodoBox.jsx
@@ -6,6 +6,30 @@ class TodoBox extends React.Component {
 
     static Item = Item;
 
+
+    render() {
+        return (
+            <div>
+                <div className="mb-3">
+
+                    <form className="d-flex">
+                        <div className="me-3">
+                            <input type="text" defaultValue="" required="" className="form-control"
+                                   placeholder="I am going..."/>
+                        </div>
+
+                        <button type="submit" className="btn btn-primary">add</button>
+                    </form>
+
+
+                </div>
+            </div>
+
+
+        )
+    }
+
+
 }
 
 export default TodoBox;
