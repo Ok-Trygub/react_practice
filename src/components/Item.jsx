f1397e462124c1fbbf463777e0b547861de32b29
Oksana Trygub <ok.trygub@gmail.com>
2022-06-30 09:02:44 +0300

    add component

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,6 +3,25 @@ import React from 'react';
 class Item extends React.Component {
 
 
+render(){
+    return(
+
+        <React.Fragment>
+
+            <div>
+                <div className="row">
+                    <div className="col-auto">
+                        <button type="button" className="btn btn-primary btn-sm">-</button>
+                    </div>
+                    <div className="col">{this.props.task}</div>
+                </div>
+                <hr />
+            </div>
+
+        </ React.Fragment>
+
+    )
+}
 }
 
 export default Item;
--- src/components/TodoBox.jsx
+++ src/components/TodoBox.jsx
@@ -1,35 +1,102 @@
 import React from 'react';
+import _ from 'lodash';
 import Item from "./Item";
 
 class TodoBox extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            itemId: 0,
+            task: ''
+        }
+        this.dbKey = 'taskList';
+    }
 
     static Item = Item;
 
+    handleChange = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
 
-    render() {
-        return (
-            <div>
-                <div className="mb-3">
+        const {target} = event;
+        this.setState(state => ({task: target.value}));
 
-                    <form className="d-flex">
-                        <div className="me-3">
-                            <input type="text" defaultValue="" required="" className="form-control"
-                                   placeholder="I am going..."/>
-                        </div>
+        // setTimeout(() => console.log(this.state.task), 1000)
+    }
 
-                        <button type="submit" className="btn btn-primary">add</button>
-                    </form>
+    addTask = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
 
+        if (!this.state.task.length) return;
 
-                </div>
-            </div>
+        const uniqueId = _.uniqueId();
 
+        this.setState(state => ({currentItemId: uniqueId}));
 
-        )
+        let data = {
+            itemId: uniqueId,
+            task: this.state.task
+        }
+        console.log(data)
+
+        this.setData(data);
     }
 
 
+    setData(todoItemData) {
+        if (!this.hasItem()) {
+            this.setItem([todoItemData]);
+            return;
+        }
+
+        const storageData = JSON.parse(
+            localStorage.getItem(this.dbKey)
+        )
+
+        const currentData = [...storageData, todoItemData];
+        this.setItem(currentData);
+    }
+
+    hasItem() {
+        let data = localStorage.getItem(this.dbKey);
+        if (data === null) return false;
+
+        return true;
+    }
+
+    setItem(data) {
+        return localStorage.setItem(
+            this.dbKey,
+            JSON.stringify(data)
+        );
+    }
+
+    renderItem() {
+        return <Item task={this.state.task}/>
+    }
+
+
+    render() {
+
+
+        return (
+            <div>
+                <div className="mb-3">
+                    <form className="d-flex">
+                        <div className="me-3">
+                            <input type="text" value={this.state.task} onChange={this.handleChange}
+                                   required="" className="form-control" placeholder="I am going..."/>
+                        </div>
+                        <button type="submit" className="btn btn-primary" onClick={this.addTask}>add</button>
+                    </form>
+                </div>
+
+                {this.renderItem()}
+            </div>
+        )
+    }
 }
 
 export default TodoBox;
