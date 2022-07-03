658958e124b097728cb9e64e569b7515a1f2143f
Oksana Trygub <ok.trygub@gmail.com>
2022-07-03 17:39:25 +0300

    add PostCatalog.jsx

--- src/components/PostCatalog.jsx
+++ src/components/PostCatalog.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import PostItem from "./PostItem";
 
-function PostCatalog(){
+function PostCatalog() {
+
+    const [posts, setPosts] = useState([]);
+    console.log(posts)
+
+    useEffect(() => {
+        async function getPosts() {
+            const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+            let data = await res.json();
+            console.log(data)
+
+            if (!data) return;
+            setPosts(data);
+        }
+
+        getPosts();
+    }, [])
 
 
-
-
-
+    return (
+        <div className="posts">
+            <ul className="posts__list">
+                {posts.map(item => <PostItem post={item} key={item.id}/>)}
+            </ul>
+        </div>
+    )
 }
-
 
 export default PostCatalog;
