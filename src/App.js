2b82f21f676470a0f2594e66aa19f7334c259f9d
Oksana Trygub <ok.trygub@gmail.com>
2022-07-04 08:44:41 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
 import MarkdownEditor from "./components/MarkdownEditor";
 
 function App() {
 
     return (
         <div className="App">
-            <MarkdownEditor/>
+
+            Из задания: Компонент принимает на вход функцию как свойство onContentChange, которая вызывается при каждом
+            изменении в редакторе.
+
+            Эту ф-ию надо прописать тут прямо в скобках?
+
+            или внутри компонента? тогда как ее отсюда вызвать?
+
+            что она должна делать с контентом?
+            <MarkdownEditor onContentChange={console.log}/>
         </div>
     );
 }
--- src/components/MarkdownEditor.jsx
+++ src/components/MarkdownEditor.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import Editor from '@toast-ui/editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
 
-function MarkdownEditor(){
+function MarkdownEditor() {
+    const editor = new Editor({
+        el: document.querySelector('#editor'),
+        hideModeSwitch: true,
+    });
+
+    // addHook - что за метод?
+    editor.addHook('change', () => {
+        const content = editor.getMarkdown();
+        console.log(content)
+
+        // Зачем нужна ф-я onContentChange, если внутри этой функции будет, как ты написал
+        // код который будет вызван при изменении содержимого редактора
+
+
+        function contentHandler(content) {
+            console.log(content)
+        }
 
 
 
-
+    });
 }
 
 export default MarkdownEditor;
