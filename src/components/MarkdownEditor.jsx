98830dd789f5e14f4a3ae76d58a4569f4d4ccb06
Oksana Trygub <ok.trygub@gmail.com>
2022-07-08 15:25:11 +0300

    add MardownEditor component

--- src/components/MarkdownEditor.jsx
+++ src/components/MarkdownEditor.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
+import {useRef, useEffect} from "react";
 import Editor from '@toast-ui/editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
-function MarkdownEditor() {
-    const editor = new Editor({
-        el: document.querySelector('#editor'),
-        hideModeSwitch: true,
-    });
+function MarkdownEditor(props) {
+    const elem = useRef(null);
+    console.log(elem)
 
-    // addHook - что за метод?
-    editor.addHook('change', () => {
-        const content = editor.getMarkdown();
-        console.log(content)
+    useEffect(() => {
+        const editor = new Editor({
+            el: elem.current,
+            hideModeSwitch: true,
+        });
+        console.log(editor)
 
-        // Зачем нужна ф-я onContentChange, если внутри этой функции будет, как ты написал
-        // код который будет вызван при изменении содержимого редактора
+        editor.addHook('change', props.onContentChange);
+    })
 
-
-        function contentHandler(content) {
-            console.log(content)
-        }
-
-
-
-    });
+    return (
+        <div id="editor" ref={elem}></div>
+    )
 }
 
 export default MarkdownEditor;
